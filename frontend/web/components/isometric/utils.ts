37a04dc875278b6bf7def0b38ba8a5e92cc51d9d
hironow <hironow365@gmail.com>
2025-01-17 06:18:01 +0900

    Refactor consoleLogWithStyle function in utils.ts

--- frontend/web/components/isometric/utils.ts
+++ frontend/web/components/isometric/utils.ts
@@ -3,7 +3,5 @@ export const consoleLogWithStyle = (message: string) => {
     message,
     'color:gray; background-color:yellow; padding:2px; border-radius:4px;',
     '',
-    'color:green;',
-    '',
   );
 };
