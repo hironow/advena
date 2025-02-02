cac02754ee0a4e2c68d393bc70d2d372f489c005
hironow <hironow365@gmail.com>
2025-02-02 21:54:08 +0900

    update pytest

--- backend/genai/tests/conftest.py
+++ backend/genai/tests/conftest.py
@@ -1,3 +1,20 @@
 import os
 
-os.environ["WANDB_DISABLED"] = "true"  # wandb を完全無効化
+import pytest
+
+from src.logger import logger
+
+
+def pytest_configure(config):
+    # pytest開始時に一度だけ呼び出されるフック
+    logger.info("Pytest is starting up...")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def disable_wandb_and_weave():
+    os.environ["WANDB_DISABLED"] = "true"
+    logger.info("WANDB_DISABLED set to 'true'")
+
+    import weave
+
+    logger.info("Weave is imported")
