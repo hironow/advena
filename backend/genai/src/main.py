057b3410fbd0e4db7ec0a3b2eb8b444d64620f34
hironow <hironow365@gmail.com>
2025-02-02 22:11:31 +0900

    Remove unused code related to agents configuration

--- backend/genai/src/main.py
+++ backend/genai/src/main.py
@@ -23,51 +23,51 @@ else:
     weave.init(project_name=os.getenv("WEAVE_PROJECT_NAME", ""))
     L.initialize(project_api_key=os.getenv("LMNR_PROJECT_API_KEY"))
 
+# agents
+# # see: https://docs.livekit.io/agents/integrations/google/#gemini-llm
+# llm = livekit_google.LLM(
+#     model="gemini-2.0-flash-exp",
+#     candidate_count=1,
+#     temperature=0.08,
+#     vertexai=True,
+#     tool_choice="auto",  # NOTE: 動的に変えたい required, none
+# )
 
-# see: https://docs.livekit.io/agents/integrations/google/#gemini-llm
-llm = livekit_google.LLM(
-    model="gemini-2.0-flash-exp",
-    candidate_count=1,
-    temperature=0.08,
-    vertexai=True,
-    tool_choice="auto",  # NOTE: 動的に変えたい required, none
-)
+# tts = livekit_google.TTS(
+#     language="ja-JP",
+#     gender="female",
+#     voice_name="ja-JP-Neural2-B",  # use Neural2 voice type: ja-JP-Neural2-C, ja-JP-Neural2-D see: https://cloud.google.com/text-to-speech/docs/voices
+#     encoding="linear16",
+#     effects_profile_id="large-automotive-class-device",
+#     sample_rate=24000,
+#     pitch=0,
+#     speaking_rate=1.0,
+# )
 
-tts = livekit_google.TTS(
-    language="ja-JP",
-    gender="female",
-    voice_name="ja-JP-Neural2-B",  # use Neural2 voice type: ja-JP-Neural2-C, ja-JP-Neural2-D see: https://cloud.google.com/text-to-speech/docs/voices
-    encoding="linear16",
-    effects_profile_id="large-automotive-class-device",
-    sample_rate=24000,
-    pitch=0,
-    speaking_rate=1.0,
-)
+# stt = livekit_google.STT(
+#     languages="ja-JP",
+#     detect_language=True,
+#     interim_results=True,
+#     punctuate=True,
+#     spoken_punctuation=True,
+#     model="chirp_2",
+#     sample_rate=16000,
+#     keywords=[
+#         ("mi-ho", 24.0),  # 仮設定
+#     ],
+# )
 
-stt = livekit_google.STT(
-    languages="ja-JP",
-    detect_language=True,
-    interim_results=True,
-    punctuate=True,
-    spoken_punctuation=True,
-    model="chirp_2",
-    sample_rate=16000,
-    keywords=[
-        ("mi-ho", 24.0),  # 仮設定
-    ],
-)
-
-model = livekit_google.beta.realtime.RealtimeModel(
-    model="gemini-2.0-flash-exp",
-    voice="Charon",
-    modalities=["TEXT", "AUDIO"],
-    enable_user_audio_transcription=True,
-    enable_agent_audio_transcription=True,
-    vertexai=True,
-    candidate_count=1,
-    temperature=0.08,
-    instructions="You are a helpful assistant",
-)
+# model = livekit_google.beta.realtime.RealtimeModel(
+#     model="gemini-2.0-flash-exp",
+#     voice="Charon",
+#     modalities=["TEXT", "AUDIO"],
+#     enable_user_audio_transcription=True,
+#     enable_agent_audio_transcription=True,
+#     vertexai=True,
+#     candidate_count=1,
+#     temperature=0.08,
+#     instructions="You are a helpful assistant",
+# )
 
 # FastAPI アプリ作成
 app = FastAPI()
