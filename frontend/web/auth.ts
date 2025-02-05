2f3a551a2259c4e91b6b453e3f93286ac275daf2
hironow <hironow365@gmail.com>
2025-02-05 22:35:02 +0900

    bugfix auth

--- frontend/web/auth.ts
+++ frontend/web/auth.ts
@@ -15,6 +15,7 @@ interface ExtendedSession extends Session {
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   debug: true,
+  trustHost: true,
   providers: [
     // Google,
     // NOTE: Google Providerを使うとfirebase auth emulatorで動作しない。カスタム認証を使う
