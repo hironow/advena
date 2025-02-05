f80d66193e64d83a7f460d057ede56f911d614d6
hironow <hironow365@gmail.com>
2025-02-05 16:26:37 +0900

    add dev and prd env

--- frontend/web/lib/firebase/client.ts
+++ frontend/web/lib/firebase/client.ts
@@ -24,7 +24,6 @@ function createFirebaseClientApp(): ReturnType<typeof initializeClientApp> {
   const options: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
