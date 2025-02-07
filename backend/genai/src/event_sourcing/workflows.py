7ada93061f4f7c7f1b4ea408dd25617fc9eaff5c
hironow <hironow365@gmail.com>
2025-02-08 03:17:23 +0900

    add cmt

--- backend/genai/src/event_sourcing/workflows.py
+++ backend/genai/src/event_sourcing/workflows.py
@@ -7,3 +7,16 @@
 # * GCS -> Firestore
 # * Firestore -> GCS
 # * Firestore -> LLM
+#
+# GCSのバケット以下のフォルダを定義する:
+# * rss (raw): lastBuildDateと検索キーワードを末尾に含めたファイル (更新される可能性がないので、削除しない)     : /rss/{lastBuildDate}_{keyword}.xml
+# * oai_pmh (raw): 1書籍ごとのデータ (更新されている可能性があるので、古いものから除去して良い)                : /oai_pmh/{book_id}.xml
+# * combined masterdata (edited): rssとoai_pmhを結合して、ラジオ番組作成が可能な形式に変換したファイル      : /combined_masterdata/{lastBuildDate}_{keyword}.json
+# * radio audio data (edited): ラジオ番組の音声データ                                               : /radio_audio_data/{lastBuildDate}_{keyword}.mp3
+# * radio script data (edited): ラジオ番組の音声データに対応するスクリプトデータ                        : /radio_script_data/{lastBuildDate}_{keyword}.json
+#
+# 要件:
+# RSSを取得すれば、combined masterdataを作成できる
+# combined masterdataを取得すれば、ラジオ番組を作成できる(スクリプト段階)
+# ラジオ番組(スクリプト)を作成すれば、音声データを作成できる
+
